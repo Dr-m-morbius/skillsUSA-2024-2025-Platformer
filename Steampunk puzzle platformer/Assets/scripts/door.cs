a9df76a88c0ceb8230ca031c3f061da30c228d48
Morbius <113389499+Dr-m-morbius@users.noreply.github.com>
2024-11-08 09:22:49 -0800

    attempting door

--- Steampunk puzzle platformer/Assets/scripts/door.cs
+++ Steampunk puzzle platformer/Assets/scripts/door.cs
@@ -1,23 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
+using UnityEditor;
 using UnityEngine;
 
 public class door : MonoBehaviour
 {
        public Transform doorsecondposition;
        public float Speed = 6f;
+       bool moving = false;
+       Animator m_Animator;
+       Rigidbody2D rb;
        public float waitime;
       
        public Transform originaldoorposition;
     // Start is called before the first frame update
     void Start()
     {
-      
+       m_Animator = GetComponent<Animator> ();
+       rb = GetComponent<Rigidbody2D>();
+       Vector2 Velocity = rb.velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (rb.velocity.y >= .1)
+        {
+            moving = true;
+        }
+        else if (rb.velocity.x >= .1)
+        {
+            //moving = true;
+        }
+        else
+        {
+            //moving = false;
+        }
+
+        m_Animator.SetBool ("moving", moving);
         
     }
     public void movedoor()
